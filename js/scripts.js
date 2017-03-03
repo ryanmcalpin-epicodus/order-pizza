9cd773a889a6f64ff4561dd71d0fe34f17f75526
Ryan McAlpin <rhmcalpin@gmail.com>
2017-03-03 12:51:49 -0800

    add a button to call the order constructor, calculate total price, and display price

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-function Order() {
-  this.pizzas = [];
+function Order(pizzas) {
+  this.pizzas = pizzas;
 
 }
 
@@ -11,25 +11,30 @@ function Pizza(size, toppings) {
 Pizza.prototype.getPrice = function() {
   var basePrice;
   switch (this.size) {
-    case "xl":
-      basePrice = 20;
-      break;
-    case "lg":
-      basePrice = 15;
-      break;
-    case "md":
-      basePrice = 12;
-      break;
-    case "sm":
-      basePrice = 10;
-      break;
-    case "mi":
-      basePrice = 8;
-      break;
+    case "Extra Large":
+    basePrice = 20;
+    break;
+    case "Large":
+    basePrice = 15;
+    break;
+    case "Medium":
+    basePrice = 12;
+    break;
+    case "Small":
+    basePrice = 10;
+    break;
+    case "Mini":
+    basePrice = 8;
+    break;
   }
   var numberOfToppings = this.toppings.length;
   var price = (numberOfToppings * 2) + basePrice;
   return price;
+}
+
+Pizza.prototype.getToppings = function() {
+  toppings = this.toppings.join(", ");
+  return toppings;
 }
 
 Order.prototype.totalPrice = function() {
@@ -40,23 +45,34 @@ Order.prototype.totalPrice = function() {
   return total;
 }
 
+var pizzas = [];
+
 
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
-    var newOrder = new Order();
+
 
     var size = $("input:radio[name=size]:checked").val();
     var toppings = [];
     $("input:checkbox[name=toppings]:checked").each(function() {
-      toppings.push(this);
+      toppings.push(this.value);
     });
     var newPizza = new Pizza(size, toppings);
-    newOrder.pizzas.push(newPizza);
+    pizzas.push(newPizza);
+    console.log(pizzas);
+    $("ul#pies-display").append("<li>" + newPizza.size + " pie with " + newPizza.getToppings() + "</li>");
+    // $("ul#order-display").append("<li><span class='pizza'>" + newPizza.size + "</span></li>");
 
-    newOrder.pizzas.forEach(function(pizza) {
-      $("ul#order-display").append("<li><span class='pizza'>" + pizza.size + "</span></li>");
-    });
+    // $(".pizza").last().click(function() {
+    //   $("#pizza-display").text(newPizza.getToppings());
+    // });
+  });
+
+  $("#order").click(function() {
+    var newOrder = new Order(pizzas);
+    console.log(newOrder);
+    $("#order-display").text("Your total is $" + newOrder.totalPrice() + ".");
   });
 });
