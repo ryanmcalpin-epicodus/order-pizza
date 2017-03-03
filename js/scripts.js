fa18339e742c218c7d9e33bef8a643657f59d7be
Ryan McAlpin <rhmcalpin@gmail.com>
2017-03-03 15:41:04 -0800

    add error checking for adding a pizza without a size and submitting an order without any pizzaz. also grammer checking for pizza without toppings (cheese), 2 toppings (this and that), and more than 2 toppings (this, that, and the other), oxford comma in full effect.

--- js/scripts.js
+++ js/scripts.js
@@ -33,22 +33,33 @@ Pizza.prototype.getPrice = function() {
 }
 
 Pizza.prototype.getToppings = function() {
-  toppings = this.toppings.join(", ");
+  var toppings ="";
+  if (this.toppings.length > 2) {
+    var last = this.toppings.pop();
+    toppings = this.toppings.join(", ") + ", and " + last;
+  } else if (this.toppings.length === 2) {
+    toppings = this.toppings.join(" and ");
+  } else if (this.toppings.length === 1) {
+    toppings = this.toppings;
+  }
   return toppings;
 }
 
 Order.prototype.totalPrice = function() {
-  total = 0;
+  var total = 0;
   this.pizzas.forEach(function(element) {
-    total += element.getPrice();
+    if (element) {
+      total += element.getPrice();
+    }
   });
   return total;
 }
 
 
+
 //UI LOGIC
 var pizzas = [];
-var ind = -1;
+var ind = 0;
 var clearInputs = function() {
   $("input").prop("checked", false);
 }
@@ -56,34 +67,63 @@ var clearPriceDisplay = function() {
   $("#order-display").text("");
 }
 
+var makeOrderText = function(price, amount) {
+  var txt;
+  if (amount === 0) {
+    alert("You haven't added any pizzas to your order!")
+  } else if (amount === 1) {
+    txt = "Your total is $" + price + " for " + amount + " pizza pie.";
+  } else {
+    txt = "Your total is $" + price + " for " + amount + " pizza pies.";
+  }
+  return txt;
+}
+
+var pizzaDescription = function(size, toppings) {
+  var description;
+  if (!toppings) {
+    description = "<li>" + size + " cheese pie</li>"
+  } else {
+    description = "<li>" + size + " pie with " + toppings + "</li>"
+  }
+  return description;
+}
+
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
+
     var size = $("input:radio[name=size]:checked").val();
     var toppings = [];
-    $("input:checkbox[name=toppings]:checked").each(function() {
-      toppings.push(this.value);
-    });
-    var newPizza = new Pizza(size, toppings);
-    pizzas.push(newPizza);
-    console.log(pizzas);
-    $("ul#pies-display").append("<li>" + newPizza.size + " pie with " + newPizza.getToppings() + " <span class='pizza'>[x]</span></li>");
+    if (!size) {
+      alert("Please select a size.");
+    } else {
+      $("input:checkbox[name=toppings]:checked").each(function() {
+        toppings.push(this.value);
+      });
+      var newPizza = new Pizza(size, toppings);
+      pizzas.push(newPizza);
+      console.log(pizzas);
+      var description = pizzaDescription(newPizza.size, newPizza.getToppings());
+      $("ul#pies-display").append(description);
 
-    ind++
-    $(".pizza").last().click(function() {
-      console.log(this);
-      $(this).parent().remove();
-      pizzas.splice(ind, 1);
-    });
-    clearInputs();
+      clearInputs();
+      clearPriceDisplay();
+    }
+  });
+
+  $("#remove").click(function() {
+    pizzas.pop();
+    $("li").last().remove();
     clearPriceDisplay();
   });
 
   $("#order").click(function() {
     var newOrder = new Order(pizzas);
     console.log(newOrder);
-    $("#order-display").text("Your total is $" + newOrder.totalPrice() + ".");
+    var displayMessage = makeOrderText(newOrder.totalPrice(), newOrder.pizzas.length);
+    $("#order-display").text(displayMessage);
     clearInputs();
   });
 });
